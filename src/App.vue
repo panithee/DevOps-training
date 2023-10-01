ce64678de89f094036d0a5cb89406749efdb05a0
Student <Student@5CD3019B1D.control.ad.sit.kmutt.ac.th>
2023-10-01 13:53:11 +0700

    template

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex w-screen h-screen bg-green-200">
+    <div class="mx-3 flex w-full h-2/3 justify-center m-auto bg-cyan-200">
+    <div class="flex w-2/3 h-2/3 justify-center m-auto bg-red-300">
+     <p class="text-3xl text-center m-auto">Hello World ....</p>
+    </div>
+     
+    </div>
+  </div>
+ 
+</template>
+
+<style scoped>
+
+</style>
