a82cca455c4bb68466a07be392c2da18eccadc86
Student <Student@5CD3019B1D.control.ad.sit.kmutt.ac.th>
2023-10-01 14:55:34 +0700

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <div class="mx-3 flex w-full h-2/3 justify-center m-auto bg-cyan-200">
     <div class="flex w-2/3 h-2/3 justify-center m-auto bg-red-300">
      <p class="text-3xl text-center m-auto">Hello World ....</p>
+     <p>Hello</p>
     </div>
      
     </div>
